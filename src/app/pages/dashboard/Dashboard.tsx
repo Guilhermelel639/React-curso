52918697a3cbaa362b140a0f3f4cee7628e2c44e
Guilhermelel639 <guilherme.giampaolo@gmail.com>
2025-12-10 14:32:56 -0300

    feat: lista

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,15 +1,33 @@
-import { Link } from "react-router-dom";
-import { useUsuarioLogado } from "../../shared/hooks";
+import {use, useCallback, useState} from 'react';
 
 export const Dashboard = () => {
-  const { nomeDoUsuario, logout } = useUsuarioLogado();
+  const [lista, setLista] = useState<string[]>(["teste1", "teste2", "teste3"]);
+
+  const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+    if (e.key === "Enter") {
+      if (e.currentTarget.value.trim().length === 0) return;
+
+      const value = e.currentTarget.value;
+
+      setLista((prevLista) => {
+        if (prevLista.includes(value))return prevLista;
+        return [...prevLista, value];
+      });
+      e.currentTarget.value = "";
+    }
+  }, []);
 
   return (
     <div>
-      <p>Dashboard Page</p>
-      <p>{nomeDoUsuario}</p>
-      <Link to="/login">Go to Login</Link>
-      <button onClick={logout}>Logout</button>
+      <p>lista</p>
+      <input type="text" onKeyDown={handleKeyDown} />
+
+      <ul>
+        {lista.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+
     </div>
   );
 };
